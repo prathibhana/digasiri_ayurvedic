7efd8e3d9db99e89f9ab3492c4add6ac4fda8747
prathibhana <spprathibhana@gmail.com>
2020-05-17 12:13:49 +0530

    data view and insert. view and add user

---
+++ application/controllers/users/Users.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Users extends CI_Controller
+{
+	public function __construct() //construct for load data from start
+	{ parent:: __construct();
+		$this->load->model('UsersModel');
+	}
+
+	public function index()
+	{
+		$data = array(
+			'users'=>$this->UsersModel->select()
+		);
+
+		$this->load->view('header');
+		$this->load->view('users/users',$data);
+		$this->load->view('footer');
+	}
+	public function add_user(){
+		$new_user=array(
+			'first_name'=>$this->input->post('first_name'),
+			'last_name'=>$this->input->post('last_name'),
+			'email'=>$this->input->post('email'),
+			'create_date'=>date('Y-m-d')
+		);
+		$result = $this->UsersModel->create($new_user);
+
+		if ($result ==true){
+			redirect('users/users');
+		}
+
+	}
+}
---
+++ application/views/users/users.php
@@ -0,0 +1,85 @@
+<!-- Main content -->
+<section class="content">
+	<div class="row">
+		<div class="col-md-12">
+			<!--
+				Empty box
+				<div class="box">
+				<div class="box-header"></div>
+				<div class="box-body"></div>
+				<div class="box-footer"></div>
+			</div> -->
+			<div class="box">
+				<div class="box-header">
+					<h3 class="box-title">USERS </h3>
+					<div class="text-right">
+						<button type="button" class="btn btn-success" data-toggle="modal" data-target="#add_user_modal">
+							Add New User
+						</button>
+					</div>
+				</div>
+				<div class="box-body"></div>
+				<table class="table table-hover">
+					<tr>
+						<th>user_name</th>
+						<th>Email</th>
+						<th>Create Date</th>
+						<th>Status</th>
+
+					</tr>
+				</table>
+				<div class="box-footer"></div>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<!--add new user modal-->
+<div class="modal fade" tabindex="-1" role="dialog" id="add_user_modal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+
+			<div class="modal-header">
+				<div class="modal-title"><h4>New User</h4></div>
+
+			</div>
+			<form action="<?php base_url();?>users/add_user" method="post">
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>First Name</label>
+								<input type="text" class="form-control" name="first_name" id="first_name"
+									   placeholder="First Name" data-validation="required">
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Last Name</label>
+								<input type="text" class="form-control" name="last_name" id="last_name"
+									   placeholder="Last Name" data-validation="required">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="form-group">
+								<label>Email Address</label>
+								<input type="email" class="form-control" name="email" id="email" placeholder="Email"
+									   data-validation="email">
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<div class="text-right">
+						<button type="reset" class="btn btn-success" data-toggle="modal" data-target="">Reset</button>
+						<button type="submit" class="btn bg-aqua" data-toggle="modal" data-target="">Create</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
